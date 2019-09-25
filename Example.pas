bcc0283abe21f47a2455ef39780bf5957d2117d6
kimhyunjin <khjzzm@gmail.com>
2019-09-25 16:29:01 +0900

    added IPRestrictOnOff configurationg

--- Example.pas
+++ Example.pas
@@ -2,7 +2,7 @@
 { 팝빌 전자세금계산서 API Delphi SDK Example
 {
 { - 델파이 SDK 적용방법 안내 : http://blog.linkhub.co.kr/572
-{ - 업데이트 일자 : 2019-09-16
+{ - 업데이트 일자 : 2019-09-25
 { - 기술지원 연락처 : 1600-9854 / 070-4304-2991
 { - 기술지원 이메일 : code@linkhub.co.kr
 {
@@ -265,6 +265,9 @@ begin
 
         //Exception 처리 설정값. 미기재시 true(기본값)
         taxinvoiceService.IsThrowException := true;
+
+        //인증토큰 IP제한기능 사용여부, true(권장-사용), false(미사용)
+        taxinvoiceService.IPRestrictOnOff := false;        
 end;
 
 procedure TfrmExample.FormClose(Sender:TObject; var Action:TCloseAction);
